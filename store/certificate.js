16d3a6ce45548e676d4e63a9265d227177ca75e2
betuah <betuahanugerah@gmail.com>
2022-01-10 01:00:04 +0700

    certificate

---
+++ store/certificate.js
@@ -0,0 +1,30 @@
+export const state = () => ({
+    data: [],
+})
+
+export const mutations = {
+    STORE_CERTIFICATE_DATA(state, data) {
+        state.data = data
+    }
+}
+
+export const actions = {
+    async getAll({commit}, data) {
+        try {
+            const res = await this.$axios.get('/api/certification')
+            await commit('STORE_CERTIFICATE_DATA', res.data.data)
+
+            if (res.data.code === 'OK') return res.data.data
+            else return { code: res.data.code , error: res.data.status}
+        } catch (error) {
+            return []
+        }
+        
+    }
+}
+
+export const getters = {
+    getData(state) {
+        return state.data
+    }
+}
