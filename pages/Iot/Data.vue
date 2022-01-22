ebae3405c49dc088a946e4e698dddeaee969e94d
betuah <betuahanugerah@gmail.com>
2022-01-22 12:35:50 +0700

    add data management

--- pages/Iot/Data.vue
+++ pages/Iot/Data.vue
@@ -1,5 +1,9 @@
 <template>
     <v-container fill-height>
+        <AlertConfirm ref="confirm"/>
+
+        <Notif ref="notif" />
+
         <v-card elevation="3" class="col-12 mx-auto pa-0 rounded-lg">
             <div class="d-flex flex-column indigo lighten-2 pa-0">
                 <div class="d-flex flex-column-reverse flex-md-row flex-md-row px-5 py-4">
@@ -14,63 +18,129 @@
                                 restore
                             </v-icon>
                         </v-btn>
-                        <v-btn small color="white elevation-4" outlined class="white--text" @click="goBack()">
+                        <v-btn small color="white" outlined @click="goBack()">
                             <v-icon left>
                                 arrow_back
                             </v-icon>
-                            KEMBALI
+                            back
                         </v-btn>
                     </div>
                 </div>
-                <div class="col-12 px-5 pt-0 indigo lighten-3">
+                <div class="d-flex align-end indigo px-5 py-3 pt-1 lighten-3">
                     <v-text-field
-                    v-model="search"
-                    label="Cari"
-                    append-icon="search"
-                    dark
-                    hide-details
+                        v-model="search"
+                        label="Search anything"
+                        append-icon="search"
+                        dark
+                        hide-details
                     ></v-text-field>
+                    <v-tooltip bottom>
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-badge
+                                bordered
+                                color="success"
+                                :content="selectedRows.length"
+                                :value="(selectedRows.length)"
+                                overlap
+                            >
+                                <v-btn 
+                                    v-bind="attrs" 
+                                    v-on="on" 
+                                    :class="selectedRows.length > 0 ? 'd-flex ml-5 success--text' : 'd-none'" 
+                                    color="white" 
+                                    small
+                                    @click="genAnyCertificate()"
+                                >
+                                    <v-icon color="green" small left>
+                                        verified
+                                    </v-icon>
+                                    Generate
+                                </v-btn>
+                            </v-badge>
+                        </template>
+                        <span class="text-caption">Generate Certificate</span>
+                    </v-tooltip>
                 </div>
             </div>
             <v-data-table
+                v-model="selectedRows"
                 :headers="headers"
                 :items="desserts"
                 :search="search"
                 :loading="loading"
+                fixed-header
+                multi-sort
+                justify="space-around"
                 loading-text="Mengambil data... Mohon Tunggu!"
                 no-data-text="Data tidak tersedia."
                 no-results-text="Data tidak ditemukan."
                 class="grey--text"
             >
-                <template v-slot:[`item.paymentStats`]="{ item }">
-                    <v-chip
-                        small
-                        class="pa-3 white--text text-caption font-weight-light text-capitalize"
-                        :color="item.paymentStats == '1' ? 'warning lighten-1' : ( item.paymentStats == '2' ? 'green lighten-1' : 'red lighten-1')"
-                    >
-                        {{ item.paymentStats == '1' ? 'Process' : ( item.paymentStats == '2' ? 'Accepted' : 'Reject') }}
-                    </v-chip>
-                </template>
-                <template v-slot:[`item.completion`]="{ item }">
-                    <v-chip
-                        small
-                        class="pa-3 white--text text-caption font-weight-light text-capitalize"
-                        :color="item.completion == 0 ? 'warning lighten-1' : 'green lighten-1'"
-                    >
-                        {{ item.completion == 0 ? 'Not Complete' : 'Completed' }}
-                    </v-chip>
-                </template>
-                <template v-slot:[`item.fullName`]="{ item }">
-                    <span class="success--text font-weight-medium text-body-2 text-caption text-capitalize">{{item.fullName}}</span>
-                </template>
-                <template v-slot:[`item.courseType`]="{ item }">
-                    <span class="grey--text font-weight-medium text-body-2 text-caption text-capitalize">{{item.courseType}}</span>
-                </template>
-                <template v-slot:[`item.title`]="{ item }">
-                    <span class="success--text font-weight-medium text-body-2 text-caption text-capitalize">{{item.title}}</span>
-                </template>
-                <template v-slot:[`item.email`]="{ item }">
-                    <span class="grey--text font-weight-medium text-body-2 text-caption">{{hideEmail(item.email)}}</span>
+                <template v-slot:item="{ item }">
+                    <tr :class="selectedRows.indexOf(item.participantId)>-1 ? 'blue lighten-5 rounded-0 white--text' : ''">
+                        <td class="d-flex align-center" @click ="toggle(item.participantId)">
+                            <span class="success--text font-weight-medium text-body-2 text-caption text-capitalize">{{item.fullName}}</span>
+                            <v-icon 
+                                class="ml-2"
+                                small
+                                color="light-blue lighten-2" 
+                                v-if="selectedRows.indexOf(item.participantId)>-1"
+                            >
+                                check
+                            </v-icon>
+                        </td>
+                        <td @click ="toggle(item.participantId)">
+                            <span class="success--text font-weight-medium text-body-2 text-caption text-capitalize">{{item.title}}</span>
+                        </td>
+                        <td @click ="toggle(item.participantId)">
+                            <span class="grey--text font-weight-medium text-body-2 text-caption">{{hideEmail(item.email)}}</span>
+                        </td>
+                        <td @click ="toggle(item.participantId)">
+                            <v-chip
+                                small
+                                class="pa-3 white--text text-caption font-weight-light text-capitalize"
+                                :color="item.paymentStats == '1' ? 'warning lighten-1' : ( item.paymentStats == '2' ? 'green lighten-1' : 'red lighten-1')"
+                            >
+                                {{ item.paymentStats == '1' ? 'Process' : ( item.paymentStats == '2' ? 'Accepted' : 'Reject') }}
+                            </v-chip>
+                        </td>
+                        <td @click ="toggle(item.participantId)">
+                            <v-chip
+                                small
+                                class="pa-3 white--text text-caption font-weight-light text-capitalize"
+                                :color="item.completion == 0 ? 'warning lighten-1' : 'green lighten-1'"
+                            >
+                                {{ item.completion == 0 ? 'Not Complete' : 'Completed' }}
+                            </v-chip>
+                        </td>
+                        <td>
+                            <div class="d-flex flex-row justify-center align-center">
+                                <v-tooltip bottom>
+                                    <template v-slot:activator="{ on, attrs }">
+                                        <v-btn 
+                                            :class="selectedRows.length > 0 ? 'd-none' : 'd-flex'" 
+                                            v-bind="attrs" 
+                                            v-on="on" 
+                                            small 
+                                            icon
+                                            @click="genCertificate(`${item.participantId}`)"
+                                        >
+                                            <v-icon small :color="item.completion == 0 ? 'grey' : 'green'">verified</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <span class="text-caption">Generate Certificate</span>
+                                </v-tooltip>
+                                <v-tooltip bottom>
+                                    <template v-slot:activator="{ on, attrs }">
+                                        <v-btn v-bind="attrs" v-on="on" small icon @click="deleteParticipant(`${item.participantId}`)">
+                                            <v-icon small color="red">delete</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <span class="text-caption">Delete From Course</span>
+                                </v-tooltip>
+                            </div>
+                        </td>
+                    </tr>
                 </template>
             </v-data-table>
         </v-card>
@@ -78,29 +148,100 @@
 </template>
 
 <script>
+    import AlertConfirm from '../../components/AlertConfirm.vue'
+
     export default {
+        components: { AlertConfirm },
         data: () => ({
-        search: '',
-        loading: false,
-        headers: [
-            { text: 'Full Name', value: 'fullName', class: "secondary--text font-weight-bold" },
-            { text: 'Course', value: 'title', class: "" },
-            { text: 'E-Mail', value: 'email', class: "" },
-            { text: 'Payment Stats', value: 'paymentStats', class: "" },
-            { text: 'Completion', value: 'completion', class: "secondary--text font-weight-bold" }
-        ],
-        desserts: [],
-        editedIndex: -1,
-        detailsItem: {},
-        defaultItem: {
-            fullName: '',
-            code: 0,
-        },
+            search: '',
+            select: false,
+            selectedRows: [],
+            loading: false,
+            headers: [
+                { text: 'Full Name', value: 'fullName', class: "secondary--text font-weight-bold", sortable: false  },
+                { text: 'Course', value: 'title', class: "", sortable: false },
+                { text: 'E-Mail', value: 'email', class: "", sortable: false },
+                { text: 'Payment Stats', value: 'paymentStats', class: "", sortable: false },
+                { text: 'Completion', value: 'completion', class: "secondary--text font-weight-bold", sortable: false },
+                { text: 'Actions', value: 'actions', class: "secondary--text font-weight-bold", sortable: false }
+            ],
+            desserts: [],
+            editedIndex: -1,
+            detailsItem: {},
+            defaultItem: {
+                fullName: '',
+                code: 0,
+            },
         }),
         async mounted() {
             await this.getData()
         },
         methods: {
+            async deleteParticipant(id) {
+                const confirm = await this.$refs.confirm.show(
+                    'Are you sure to delete this data ?',
+                    'Your data will be permanently deleted!'
+                )
+
+                if (confirm) {
+                    try {
+                        this.loading = true
+                        await this.$axios.delete(`/api/course/enroll/${id}`)
+                        this.$refs.notif.show('success', 'Delete data success!')
+                        this.alertConfirm = false
+                        this.refreshData()
+                    } catch (e) {
+                        this.$refs.notif.show('error', `${e.response.data.code || 'Sorry your data cannot be delete.'}`)
+                        this.alertConfirm = false
+                    }
+                } else {
+                    this.alertConfirm = false
+                    this.loading = false
+                }
+            },
+            async genCertificate(id) {
+                try {
+                    this.loading = true
+                    await this.$axios.post(`/api/certificate/${id}`)
+                    this.$refs.notif.show('success', 'Generate certificate successfully!')
+                    this.alertConfirm = false
+                    this.refreshData()
+                } catch (e) {
+                    this.$refs.notif.show('error', `${e.response.data.code || 'Failed to create certifite.'}`)
+                    this.alertConfirm = false
+                    this.loading = false
+                }
+            },
+            async genAnyCertificate() {
+                try {
+                    this.loading = true
+                    if (this.sselectedRows.length > 0) {
+                        await this.$axios.post(`/api/certificate/generate/any`, {
+                            "courseId": `${process.env.NUXT_ENV_COURSE_IOT_ID}`,
+                            "participantData": [...this.selectedRows]
+                        })
+                        this.$refs.notif.show('success', 'Generate certificate successfully!')
+                        this.alertConfirm = false
+                        this.refreshData()
+                        this
+                    } else {
+                        this.$refs.notif.show('info', 'No data selected!')
+                    }
+                } catch (e) {
+                    this.$refs.notif.show('error', `${'Failed to create certificate.'}`)
+                    this.alertConfirm = false
+                    this.loading = false
+                }
+            },
+            toggle(keyID) {
+                if (this.selectedRows.includes(keyID)) {
+                    this.selectedRows = this.selectedRows.filter(
+                        selectedKeyID => selectedKeyID !== keyID
+                    );
+                } else {
+                    this.selectedRows.push(keyID);
+                }
+            },
             hideEmail(email) {
                 return email.replace(/(.{2})(.*)(?=@)/,
                 (gp1, gp2, gp3) => { 
@@ -114,11 +255,12 @@
                 this.loading = true
                 try {
                     const res  = await this.$axios.get(`/api/course/enroll/${process.env.NUXT_ENV_COURSE_IOT_ID}`)
-                    console.log(res.data.data, process.env.NUXT_ENV_COURSE_IOT_ID)
                     this.desserts = res.data.data
                     this.loading = false
                 } catch (e) {
-                    console.log(e)
+                    if ((e.response && e.response.data.code) == "ERR_DATA_EMPTY") {
+                        this.desserts = []
+                    }
                     this.loading = false
                 }
             },
