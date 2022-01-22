2896c81e3fed4c16a77e52137a8591dafdee8f6f
betuah <betuahanugerah@gmail.com>
2022-01-23 00:41:20 +0700

    Meta Course

---
+++ components/imageModal.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <v-dialog
+            v-model="visible"
+            width="unset"
+        >
+            <v-img
+                :src="`${uri}`"
+                contain
+                class="grey lighten-2"
+            ></v-img>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'imageModal',
+        data: () => ({
+            visible: false,
+            uri: ""
+        }),
+        methods: {
+            show(uri) {
+                this.visible = true
+                this.uri = uri
+            },
+            dismiss() {
+                this.visible = false
+                this.uri = ""
+            }
+        }
+    }
+</script>
