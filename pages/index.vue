cd8ea65e57e0953c038f148cae2ed213840711e3
betuah <betuahanugerah@gmail.com>
2022-06-09 14:01:17 +0700

    change iframe src

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-   <iframe allowfullscreen height="100%" width="100%" src="https://landing.technobrainlab.com/techno-brain"></iframe>
+   <iframe allowfullscreen height="100%" width="100%" src="https://placement.kemdikbud.go.id/techno-brain/"></iframe>
 </template>
