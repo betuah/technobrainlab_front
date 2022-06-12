d181249231d8bf4af2b96366650d407914ead038
betuah <betuahanugerah@gmail.com>
2022-06-12 17:49:13 +0700

    change D4t4 to aws

--- pages/D4t4.vue
+++ pages/D4t4.vue
@@ -4,12 +4,12 @@
         <ImageModal ref="image" />
         <Notif ref="notif" />
 
-        <v-card elevation="3" class="col-12 mx-auto pa-0 rounded-lg">
+        <v-card elevation="3" class="col-12 mx-auto pa-0 rounded-lg tw-border-none">
             <div class="d-flex flex-column indigo lighten-2 pa-0">
                 <div class="d-flex flex-column-reverse flex-md-row flex-md-row px-5 py-4">
                     <div class="d-flex flex-row mx-md-0 mt-5 mt-md-0 justify-center align-center">
-                        <span class="font-weight-bold text-h5 light-blue--text text--lighten-5 mr-1">META</span>
-                        <span class="font-weight-light text-h5 light-blue--text text--lighten-5">Participant Data</span>
+                        <span class="font-weight-bold text-h5 light-blue--text text--lighten-5 mr-1">Participant</span>
+                        <span class="font-weight-light text-h5 light-blue--text text--lighten-5">Data</span>
                     </div>
                     <v-spacer></v-spacer>
                     <div class="d-flex flex-row-reverse flex-md-row mb-3 ma-md-0 justify-space-between align-center">
@@ -74,37 +74,40 @@
                 loading-text="Mengambil data... Mohon Tunggu!"
                 no-data-text="Data tidak tersedia."
                 no-results-text="Data tidak ditemukan."
-                class="grey--text"
+                class="grey--text tw-border-none"
             >
                 <template v-slot:item="{ item }">
-                    <tr :class="selectedRows.indexOf(item.participantId)>-1 ? 'blue lighten-5 rounded-0 white--text' : ''">
-                        <td class="d-flex align-center" @click ="toggle(item.participantId)">
-                            <span class="success--text font-weight-medium text-body-2 text-caption text-capitalize">{{item.fullName}}</span>
+                    <tr :class="selectedRows.indexOf(item.id)>-1 ? 'blue lighten-5 rounded-0 white--text' : ''">
+                        <td class="d-flex align-center" @click ="toggle(item.id)">
+                            <span class="success--text font-weight-medium text-body-2 text-caption text-capitalize">{{item.participant.fullName}}</span>
                             <v-icon 
                                 class="ml-2"
                                 small
                                 color="light-blue lighten-2" 
-                                v-if="selectedRows.indexOf(item.participantId)>-1"
+                                v-if="selectedRows.indexOf(item.id)>-1"
                             >
                                 check
                             </v-icon>
                         </td>
-                        <td @click ="toggle(item.participantId)">
-                            <span class="success--text font-weight-medium text-body-2 text-caption text-capitalize">{{item.title}}</span>
+                        <td @click ="toggle(item.id)">
+                            <span class="success--text font-weight-medium text-body-2 text-caption text-capitalize">{{courseData.course_title}}</span>
                         </td>
-                        <td @click ="toggle(item.participantId)">
-                            <span class="grey--text font-weight-medium text-body-2 text-caption">{{item.email}}</span>
+                        <td @click ="toggle(item.id)">
+                            <span class="grey--text font-weight-medium text-body-2 text-caption">{{item.participant.email}}</span>
                         </td>
-                        <td @click ="toggle(item.participantId)">
+                        <td @click ="toggle(item.id)">
+                            <span class="orange--text font-weight-medium text-body-2 text-caption">Rp. {{item.order.gross_amount}}</span>
+                        </td>
+                        <td @click ="toggle(item.id)">
                             <v-chip
                                 small
                                 class="pa-3 white--text text-caption font-weight-light text-capitalize"
-                                :color="item.paymentStats == '1' ? 'warning lighten-1' : ( item.paymentStats == '2' ? 'green lighten-1' : 'red lighten-1')"
+                                :color="item.order.payment_status == '1' ? 'green lighten-1' : 'warning lighten-1'"
                             >
-                                {{ item.paymentStats == '1' ? 'Process' : ( item.paymentStats == '2' ? 'Accepted' : 'Reject') }}
+                                {{ item.order.payment_status == '1' ? 'Accept' : 'Pending' }}
                             </v-chip>
                         </td>
-                        <td @click ="toggle(item.participantId)">
+                        <!-- <td @click ="toggle(item.id)">
                             <v-chip
                                 small
                                 class="pa-3 white--text text-caption font-weight-light text-capitalize"
@@ -112,10 +115,10 @@
                             >
                                 {{ item.completion == 0 ? 'Not Complete' : 'Completed' }}
                             </v-chip>
-                        </td>
+                        </td> -->
                         <td>
-                            <div class="d-flex flex-row justify-end align-center">
-                                <v-tooltip bottom>
+                            <div class="d-flex flex-row justify-center align-center">
+                                <!-- <v-tooltip bottom>
                                     <template v-slot:activator="{ on, attrs }">
                                         <v-btn 
                                             :class="selectedRows.length > 0 ? 'd-none' : 'd-flex'" 
@@ -129,23 +132,24 @@
                                         </v-btn>
                                     </template>
                                     <span class="text-caption">View Payment</span>
-                                </v-tooltip>
-                                <v-tooltip bottom v-if="item.paymentStats == 1">
+                                </v-tooltip> -->
+                                <v-tooltip bottom>
                                     <template v-slot:activator="{ on, attrs }">
                                         <v-btn 
                                             :class="selectedRows.length > 0 ? 'd-none' : 'd-flex'" 
                                             v-bind="attrs" 
                                             v-on="on" 
+                                            :disabled="item.order.payment_status == 1 ? true : false"
                                             small 
                                             icon
-                                            @click="acceptPayment(`${item.participantId}`)"
+                                            @click="acceptPayment(`${item.order.id}`)"
                                         >
                                             <v-icon small color="green">check_circle</v-icon>
                                         </v-btn>
                                     </template>
                                     <span class="text-caption">Accept Payment</span>
                                 </v-tooltip>
-                                <v-tooltip bottom v-if="item.paymentStats == 1">
+                                <!-- <v-tooltip bottom v-if="item.paymentStats == 1">
                                     <template v-slot:activator="{ on, attrs }">
                                         <v-btn 
                                             :class="selectedRows.length > 0 ? 'd-none' : 'd-flex'" 
@@ -153,14 +157,14 @@
                                             v-on="on" 
                                             small 
                                             icon
-                                            @click="rejectPayment(`${item.participantId}`)"
+                                            @click="rejectPayment(`${item.id}`)"
                                         >
                                             <v-icon small color="warning">cancel</v-icon>
                                         </v-btn>
                                     </template>
                                     <span class="text-caption">Reject Payment</span>
-                                </v-tooltip>
-                                <v-tooltip bottom>
+                                </v-tooltip> -->
+                                <!-- <v-tooltip bottom>
                                     <template v-slot:activator="{ on, attrs }">
                                         <v-btn 
                                             :class="selectedRows.length > 0 ? 'd-none' : 'd-flex'" 
@@ -168,21 +172,21 @@
                                             v-on="on" 
                                             small 
                                             icon
-                                            @click="genCertificate(`${item.participantId}`)"
+                                            @click="genCertificate(`${item.id}`)"
                                         >
                                             <v-icon small :color="item.completion == 0 ? 'grey' : 'green'">verified</v-icon>
                                         </v-btn>
                                     </template>
                                     <span class="text-caption">Generate Certificate</span>
-                                </v-tooltip>
-                                <v-tooltip bottom>
+                                </v-tooltip> -->
+                                <!-- <v-tooltip bottom>
                                     <template v-slot:activator="{ on, attrs }">
-                                        <v-btn v-bind="attrs" v-on="on" small icon @click="deleteParticipant(`${item.participantId}`)">
+                                        <v-btn v-bind="attrs" v-on="on" small icon @click="deleteParticipant(`${item.id}`)">
                                             <v-icon small color="red">delete</v-icon>
                                         </v-btn>
                                     </template>
                                     <span class="text-caption">Delete From Course</span>
-                                </v-tooltip>
+                                </v-tooltip> -->
                             </div>
                         </td>
                     </tr>
@@ -203,14 +207,17 @@
             selectedRows: [],
             loading: false,
             headers: [
-                { text: 'Full Name', value: 'fullName', class: "secondary--text font-weight-bold", sortable: false  },
+                { text: 'Full Name', value: 'course_title', class: "secondary--text font-weight-bold", sortable: false  },
                 { text: 'Course', value: 'title', class: "", sortable: false },
                 { text: 'E-Mail', value: 'email', class: "", sortable: false },
+                { text: 'Gross Amount', value: 'gross_amount', class: "", sortable: false },
                 { text: 'Payment Stats', value: 'paymentStats', class: "", sortable: false },
-                { text: 'Completion', value: 'completion', class: "secondary--text font-weight-bold", sortable: false },
+                // { text: 'Completion', value: 'completion', class: "secondary--text font-weight-bold", sortable: false },
                 { text: 'Actions', value: 'actions', class: "secondary--text font-weight-bold", sortable: false }
             ],
+            courseData: null,
             desserts: [],
+            url: process.env.NUXT_ENV_API_URL,
             editedIndex: -1,
             detailsItem: {},
             defaultItem: {
@@ -228,9 +235,8 @@
             async acceptPayment(id) {
                 try {
                     this.loading = true
-                    await this.$axios.post('/api/course/enroll/payment', { 
-                        participantId: id,
-                        paymentStats: 2 
+                    await this.$axios.post(`${this.url}/api/paid`, { 
+                        order_id: id,
                     })
                     this.$refs.notif.show('success', 'Delete data success!')
                     this.refreshData()
@@ -329,11 +335,15 @@
             },
             async getData() {
                 this.loading = true
+                console.log(process.env.NUXT_ENV_COURSE_AWS)
                 try {
-                    const res  = await this.$axios.get(`/api/course/enroll/all`)
-                    this.desserts = res.data.data
+                    const res  = await this.$axios.get(`${this.url}/api/v1/course/${process.env.NUXT_ENV_COURSE_AWS}`)
+                    console.log(res.data);
+                    this.desserts = res.data.course_participant
+                    this.courseData = res.data
                     this.loading = false
                 } catch (e) {
+                    console.log(e)
                     if ((e.response && e.response.data.code) == "ERR_DATA_EMPTY") {
                         this.desserts = []
                     }
@@ -344,7 +354,7 @@
                 this.getData()
             },
             goBack() {
-                this.$router.push('/meta')
+                this.$router.push('/aws')
             }
         }
     }
